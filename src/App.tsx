5c67fee95d227a948344ffe7917ab4aafe4dff15
sergiogomes <sergiopgomes92@gmail.com>
2020-10-08 06:44:58 -0300

    Changing project to typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
 import logo from "./logo.svg";
-import "./App.css";
+
+import "./App.scss";
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
