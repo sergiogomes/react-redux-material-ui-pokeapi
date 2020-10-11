3bec79440be79513ba7b9151dc9c15a5bfc52f21
sergiogomes <sergiopgomes92@gmail.com>
2020-10-10 22:07:49 -0300

    Completing type for pokemon result.

--- src/interfaces/IPokemon.ts
+++ src/interfaces/IPokemon.ts
@@ -3,20 +3,20 @@ import { Url } from "url";
 export interface IPokemon {
   id: number;
   name: string;
-  desc: string;
-  image: string;
+  desc: string; // not present in the pokeapi
+  image: string; // not present in the pokeapi
   base_experience: number;
   height: number;
   order: number;
   weight: number;
   is_default: boolean;
-  location_area_encounters: Url;
-  abilities: Array<any>;
-  forms: Array<any>;
-  game_indices: Array<any>;
+  location_area_encounters: string;
+  abilities: Array<ability>;
+  forms: Array<nameUrl>;
+  game_indices: Array<game_indice>;
   held_items: Array<any>;
-  moves: Array<any>;
-  species: any;
+  moves: Array<move>;
+  species: nameUrl;
   sprites: {
     back_default: string;
     back_female: string;
@@ -27,6 +27,44 @@ export interface IPokemon {
     front_shiny: string;
     front_shiny_female: string;
   };
-  stats: Array<any>;
-  types: Array<any>;
+  stats: Array<stat>;
+  types: Array<type>;
 };
+
+type nameUrl = {
+  name: string;
+  url: string;
+}
+
+type ability = {
+  ability: nameUrl;
+  is_hidden: boolean;
+  slot: number;
+}
+
+type game_indice = {
+  game_index: number;
+  version: nameUrl;
+}
+
+type move = {
+  move: nameUrl;
+  version_group_details: Array<version_group_detail>;
+}
+
+type version_group_detail = {
+  level_learned_at: number;
+  move_learn_method: nameUrl;
+  version_group: nameUrl;
+}
+
+type stat = {
+  base_stat: number;
+  effort: number;
+  stat: nameUrl;
+}
+
+type type = {
+  slot: number;
+  type: nameUrl;
+}
